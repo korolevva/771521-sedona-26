04c78d5bd2cc40f314b16cae14e7c82fe983418c
Vitaliy <korolevva88@yandex.ru>
2019-10-06 20:36:25 +0400

    Исправляет отображение попапа
    В выключеном состоянии попапа его маска мешала двигать карту

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,19 @@
 var buttonPopup = document.querySelector(".hotel-search__button-popup");
 var popup = document.querySelector(".hotel-search__popup-form");
+var popupMask = document.querySelector(".hotel-search__wrapper-mask");
 var isPopupShow = false;
 
 popup.classList.remove("hotel-search__popup-form--show");
+popupMask.classList.add("hotel-search__wrapper-mask--hidden");
 
 buttonPopup.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (!isPopupShow) {
+    popupMask.classList.remove("hotel-search__wrapper-mask--hidden");
     popup.classList.add("hotel-search__popup-form--show");
     isPopupShow = true;
   } else {
+    popupMask.classList.add("hotel-search__wrapper-mask--hidden");
     popup.classList.remove("hotel-search__popup-form--show");
     isPopupShow = false;
   }
